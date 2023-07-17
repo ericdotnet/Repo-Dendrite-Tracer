598655eac9bb309e707f64704b65a2977d5b1925
ericdotnet <ericdotmaldonado@gmail.com>
2023-07-17 10:46:04 -0400

    MaxProjectionSeries

--- src/DendriteTracer.Core/MaxProjectionSeries.cs
+++ src/DendriteTracer.Core/MaxProjectionSeries.cs
@@ -1,9 +1,59 @@
-﻿namespace DendriteTracer.Core;
+﻿using SciTIF.LUTs;
+
+namespace DendriteTracer.Core;
 
 public class MaxProjectionSeries
 {
+    private readonly SciTIF.TifFile Tif;
+    public int Length { get; }
+
     public MaxProjectionSeries(string tifFilePath)
     {
+        Tif = LoadTifWithErrorChecking(tifFilePath);
+        Length = Tif.Frames;
+    }
 
+    private static SciTIF.TifFile LoadTifWithErrorChecking(string tifFilePath)
+    {
+        SciTIF.TifFile tif = new(tifFilePath);
+
+        if (tif.Frames < 2)
+        {
+            throw new InvalidOperationException("Projection TIF file must contain multiple frames");
+        }
+
+        if (tif.Channels != 2)
+        {
+            throw new InvalidOperationException("Projection TIF file must have 2 channels");
+        }
+
+        if (tif.Slices != 1)
+        {
+            throw new InvalidOperationException("Projection TIF file must have only 1 slice");
+        }
+
+        return tif;
+    }
+
+    public (RasterSharp.Channel red, RasterSharp.Channel green) GetChannels(int frame)
+    {
+        SciTIF.Image red = Tif.GetImage(frame, 0, 0);
+        SciTIF.Image green = Tif.GetImage(frame, 0, 1);
+
+        RasterSharp.Channel redChannel = new(red.Width, red.Height, red.Values);
+        RasterSharp.Channel greenChannel = new(green.Width, green.Height, green.Values);
+
+        return (redChannel.Clone(), greenChannel.Clone());
+    }
+
+    public byte[] GetPreviewImageBytes(int frame)
+    {
+        (RasterSharp.Channel red, RasterSharp.Channel green) = GetChannels(frame);
+
+        red.Rescale();
+        green.Rescale();
+
+        RasterSharp.Image img = new(red, green, red);
+        return img.GetBitmapBytes();
     }
 }
--- src/DendriteTracer.Tests/MaxProjectionSeriesTests.cs
+++ src/DendriteTracer.Tests/MaxProjectionSeriesTests.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace DendriteTracer.Tests;
 
 public class MaxProjectionSeriesTests
@@ -6,5 +8,14 @@ public class MaxProjectionSeriesTests
     public void Test_LoadTif_ProjectionSeries()
     {
         DendriteTracer.Core.MaxProjectionSeries proj = new(SampleData.TSeriesTifPath);
+        (RasterSharp.Channel red, RasterSharp.Channel green) = proj.GetChannels(0);
+
+        int pixelCount = red.Width * red.Height;
+
+        double meanRed = red.GetValues().Sum() / pixelCount;
+        meanRed.Should().BeApproximately(422.67, .01); // determined using ImageJ
+
+        double meanGreen = green.GetValues().Sum() / pixelCount;
+        meanGreen.Should().BeApproximately(535.77, .01); // determined using ImageJ
     }
 }
