642d2d81f5cf8f8cc772382af8177754e40a73da
ericdotnet <ericdotmaldonado@gmail.com>
2023-07-17 10:10:52 -0400

    skeleton project

---
+++ src/DendriteTracer.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace DendriteTracer.Core;
+
+public class Class1
+{
+
+}
---
+++ src/DendriteTracer.Gui/Form1.cs
@@ -0,0 +1,9 @@
+namespace DendriteTracer.Gui;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
